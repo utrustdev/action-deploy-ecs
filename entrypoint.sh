76c62ed54d2c7a67a7d90d9b1b9bdc4d0dd50187
Miguel Palhas <mpalhas@gmail.com>
2019-12-17 16:24:54 +0000

    Fixing cluster name

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,8 @@ aws_role=$5
 aws_region=$6
 aws_ids="$7"
 
-case "${environment}-${cluster}" in
-  "development-webservices")
-    cluster_name="dev-webservices"
-    ;;
-
-  *)
-    cluster_name="${environment}-${cluster}"
-esac
-
 echo "running aws_config"
 /aws_config.sh "$aws_ids" $aws_role $aws_region
 
 echo "ecs_update"
-/ecs_update.sh $service $environment $cluster_name $task_definition
+/ecs_update.sh $service $environment $cluster $task_definition
